900ecb927edc6034856f3256d42b7265ba6c086d
Shadowmaple <1142319190@qq.com>
2019-02-28 21:45:34 +0800

    update

--- api/login.py
+++ api/login.py
@@ -5,36 +5,39 @@ from . import app
 
 @app.route('/api/login', method = ['POST'])
 def login():
-    param = request.get_json()
-    loginName = param.get('loginName')
-    password = param.get('password')
-    payload = {
-            'loginName':loginName,
-            'password':password,
-            }
-    session = requests.session()
-
-    url = "http://spoc.ccnu.edu.cn/userLoginController/getUserProfile"
-    rp = session.post(url, data=payload)
-    status_code = rp.json().get('msg')
-    if status_code:
-        return jsonify({
-                'code': 0,
-                'msg': 'login fall',
-                }), 400
-    else:
-        url =  "http://spoc.ccnu.edu.cn/userInfo/getUserInfo"
-        info = session.post(url).json()
-        urerInfo = info['data']['userInfoVO']['userinfo']
-        js = {
-                'code': 1,
-                'msg': 'login succeed',
-                'userInfo': {
-                    'username': loginName,
-                    'realname': userInfo.get('realname'),
-                    'userid': userInfo.get('id'),
-                    }
+    if request.method == 'POST':
+        param = request.get_json()
+        loginName = param.get('loginName')
+        password = param.get('password')
+        payload = {
+                'loginName':loginName,
+                'password':password,
                 }
-        return jsonify(js), 200
+        session = requests.session()
+
+        url = "http://spoc.ccnu.edu.cn/userLoginController/getUserProfile"
+        rp = session.post(url, data=payload)
+        status_code = rp.json().get('msg')
+        if status_code:
+            return jsonify({
+                    'code': 0,
+                    'msg': 'login fall',
+                    }), 400
+        else:
+            url =  "http://spoc.ccnu.edu.cn/userInfo/getUserInfo"
+            info = session.post(url).json()
+            urerInfo = info['data']['userInfoVO']['userinfo']
+            js = {
+                    'code': 1,
+                    'msg': 'login succeed',
+                    'userInfo': {
+                        'username': loginName,
+                        'realname': userInfo.get('realname'),
+                        'userid': userInfo.get('id'),
+                        }
+                    }
+            return jsonify(js), 200
+    else:
+        return jsonify({}), 500
 
 
