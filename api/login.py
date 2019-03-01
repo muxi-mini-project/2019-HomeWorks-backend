bd6a7a7f36f2cff002a3a5e21585768b8090bbda
Shadowmaple <1142319190@qq.com>
2019-03-01 16:40:39 +0800

    modify

--- api/login.py
+++ api/login.py
@@ -5,38 +5,35 @@ from . import app
 
 @app.route('/api/login', methods = ['POST'])
 def login():
-    if request.method == 'POST':
-        param = request.get_json()
-        loginName = param.get('loginName')
-        password = param.get('password')
-        payload = {
-                'loginName':loginName,
-                'password':password,
-                }
-        session = requests.session()
+    param = request.get_json()
+    loginName = param.get('loginName')
+    password = param.get('password')
+    payload = {
+            'loginName':loginName,
+            'password':password,
+            }
+    session = requests.session()
 
-        url = "http://spoc.ccnu.edu.cn/userLoginController/getUserProfile"
-        rp = session.post(url, data=payload)
-        status_code = rp.json().get('code')
-        if status_code:
-            return jsonify({
-                    'code': 0,
-                    'msg': 'login fall',
-                    }), 400
-        else:
-            url =  "http://spoc.ccnu.edu.cn/userInfo/getUserInfo"
-            info = session.post(url).json()
-            userInfo = info['data']['userInfoVO']['userInfo']
-            js = {
-                    'code': 1,
-                    'msg': 'login succeed',
-                    'userInfo': {
-                        'username': loginName,
-                        'realname': userInfo.get('realname'),
-                        'userid': userInfo.get('id'),
-                        }
-                    }
-            return jsonify(js), 200
+    url = "http://spoc.ccnu.edu.cn/userLoginController/getUserProfile"
+    rp = session.post(url, data=payload)
+    status_code = rp.json().get('code')
+    if status_code:
+        return jsonify({
+                'code': 0,
+                'msg': 'login fall',
+                }), 400
     else:
-        return jsonify({}), 500
+        url =  "http://spoc.ccnu.edu.cn/userInfo/getUserInfo"
+        info = session.post(url).json()
+        userInfo = info['data']['userInfoVO']['userInfo']
+        js = {
+                'code': 1,
+                'msg': 'login succeed',
+                'userInfo': {
+                    'userName': loginName,
+                    'realName': userInfo.get('realname'),
+                    'userId': userInfo.get('id'),
+                    }
+                }
+        return jsonify(js), 200
 
