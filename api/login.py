86a2348a05eea33e23d680663c8c7fce2f8125bb
Shadowmaple <1142319190@qq.com>
2019-03-02 17:49:48 +0800

    update

--- api/login.py
+++ api/login.py
@@ -6,7 +6,7 @@ from . import app
 @app.route('/api/login', methods = ['POST'])
 def login():
     param = request.get_json()
-    loginName = param.get('loginName')
+    loginName = param.get('userName')
     password = param.get('password')
     payload = {
             'loginName':loginName,
@@ -26,9 +26,11 @@ def login():
         url =  "http://spoc.ccnu.edu.cn/userInfo/getUserInfo"
         info = session.post(url).json()
         userInfo = info['data']['userInfoVO']['userInfo']
+        cookie = 'SESSION' + '=' + session.cookies.get_dict()['SESSION']
         js = {
                 'code': 1,
                 'msg': 'login succeed',
+                'cookie': cookie,
                 'userInfo': {
                     'userName': loginName,
                     'realName': userInfo.get('realname'),
