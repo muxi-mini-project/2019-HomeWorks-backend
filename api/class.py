33efdf6938ed92d44556bf0845d0c7a8359f547c
Shadowmaple <1142319190@qq.com>
2019-03-10 18:58:34 +0800

    update

--- api/class.py
+++ api/class.py
@@ -1,3 +1,4 @@
+import requests
 from flask import jsonify, request
 from . import app
 
@@ -5,15 +6,39 @@ from . import app
 def classList:
     param = request.get_json()
     userId = param.get('userId')
-    cookie = 
+    cookie = request.headers.get('cookie')
+    if cookie is None:
+        return jsonify({
+                "msg": "Invalid Cookie"
+            }), 400
+    header = {'cookie': cookie}
+    payload = {
+            'userId': userId,
+            'termCode': '201901',
+            'pageNum': 1,
+            'pageSize': 30,
+            }
 
+    url = 'http://spoc.ccnu.edu.cn/studentHomepage/getMySite'
+    session = requests.session()
+    session.cookies.set('cookies', cookie)
 
+    r = session.post(url, json = payload, headers=header)
+    data_list = r.json().get('data').get('list')
+    total = r.json().get('data').get('tital')
+    classList = []
+    for i in range(total):
+        course = {}
+        course['className'] = data_list[i]['courseName']
+        course['teacher'] = data_list[i]['teacher']
+        course['siteId'] = data_list[i]['siteId']
+        classList.append(course)
 
-
+    cookie = session.cookies.get_dict()['cookies']
     data = {
             'cookie': cookie,
             'code': 1,
-            'msg': 'success',
+            'msg': 'successfully login',
             'userId': userId,
             'total': total,
             'classList': classList,
