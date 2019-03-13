79f94f44ef230d9439027a276916d1011b8679a4
Shadowmaple <1142319190@qq.com>
2019-03-13 15:30:10 +0800

    update

--- app/api/assign.py
+++ app/api/assign.py
@@ -1,6 +1,7 @@
 from . import app
 import requests
 from flask import jsonify, request
+from ..models import User
 
 @app.route('/assignment/list/', methods=['GET'])
 def assignList():
@@ -11,14 +12,61 @@ def assignList():
     if not token:
         return jsonify({'msg': 'No token'}), 400
 
+    userId = User.get_userId_token(token)
+    if not userId:
+        return jsonify({'msg': 'Invalid token'}), 401
+    
     session = requests.session()
     session.cookies.set('cookies', cookie)
 
-    url = ''
-
-
+    header = {'cookie': cookie}
+    payload = {
+            'userId': userId,
+            'termCode': '201901',
+            'pageNum': 1,
+            'pageSize': 30,
+            }
+    url = 'http://spoc.ccnu.edu.cn/studentHomepage/getMySite'
+    r = session.post(url, json=payload, headers=header)
+    course_total = r.json().get('data').get('total')
+    total = 0
     assignList = []
+    for i in range(course_total):
+        course_list = r.json().get(data).get('list')[i]
+        siteId = course_list.get('siteId')
 
+        payload = {
+                'siteId': siteId,
+                'userId': userId,
+                'pageNum': 1,
+                'pageSize': 50,
+                }
+        assign_url = 'http://spoc.ccnu.edu.cn/assignment/getStudentAssignmentList'
+        rp = session.post(assign_url, json=payload, headers=header)
+        assign_total = rp.json().get('data').get('total')
+        if not assign_total:
+            total = total + assign_total
+            courseName = course_list.get('courseName')
+            teacher = course_list.get('teacherName')
+
+        for j in range(assign_total):
+            assignment = rp.json()['data'].get('list')[j]
+            assignName = assignment.get('title')
+            assignId = assignment.get('id')
+            status = assignment.get('status')
+            beginTime = assignment.get('begintime')
+            endTime = assignment.get('endtime')
+            assign_data = {
+                    'assignId': assignId,
+                    'assignName': assignName,
+                    'status': status,
+                    'beginTime': beginTime,
+                    'endTime': endTime,
+                    'siteId': siteId,
+                    'courseName': courseName,
+                    'teacher': teacher,
+                    }
+            assignList.append(assign_data)
 
     return jsonify({
             'msg': 'success',
